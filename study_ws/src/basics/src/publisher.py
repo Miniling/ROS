c79ffea4b76ba2fc6c7263b888f4b124cde348d0
min <bsm3925@kumoh.ac.kr>
2022-12-28 10:57:34 +0900

    Update: change annotation ko->en

--- study_ws/src/basics/src/publisher.py
+++ study_ws/src/basics/src/publisher.py
@@ -2,12 +2,13 @@
 import rospy
 from std_msgs.msg import Int32
 
-rospy.init_node('topic_publisher')  # 'topic_publisher'라는 이름으로 로스파이 초기화
+rospy.init_node('topic_publisher')  # init rospy: 'topic_publisher'
 
-pub = rospy.Publisher('counter', Int32, queue_size=10)   # 'counter'라는 이름의 토픽으로 데이터를 생성()
-rate = rospy.Rate(10)   # 전송 주기(단위: Hz)
+pub = rospy.Publisher('counter', Int32, queue_size=10)   # publish 'counter'
+rate = rospy.Rate(10)   # publishing rate(Hz)
 count = 0
 
+# Active
 while not rospy.is_shutdown():
     pub.publish(count)
     count += 1
--- study_ws/src/basics/src/subscriber.py
+++ study_ws/src/basics/src/subscriber.py
@@ -1,11 +1,16 @@
 #! /usr/bin/env python
 import rospy
-from std_msgs.msg import Int32
+from std_msgs.msg import Int32, String
+from geometry_msgs.msg import Twist
 
 def callback(msg):
     print(msg.data)
-    pass
+    cahnge_data = 'min : '+str(msg.data)  # string type data
+    pub.publish(cahnge_data)
 
-rospy.init_node('topic_publisher')  # 
-pub = rospy.Publisher('counter', Int32, callback)
+rospy.init_node('topic_subscriber')  # subscribe from publisher
+sub = rospy.Subscriber('counter', Int32, callback)
+pub = rospy.Publisher('min_topic', String, queue_size=1)
+
+# Passive
 rospy.spin()
